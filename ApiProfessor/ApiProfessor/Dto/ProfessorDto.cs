88fe97ed6ddd1c8a551b5d49fc51d2c0821b40f0
Mayan <may.santana197@gmail.com>
2024-08-31 19:34:53 -0300

    First commit

---
+++ ApiProfessor/ApiProfessor/Dto/ProfessorDto.cs
@@ -0,0 +1,14 @@
+﻿using ApiProfessor.Models;
+
+namespace ApiProfessor.Dto.Professor
+{
+    public class ProfessorDto
+    {
+        public string? Nome { get; set; }
+        public string? Cpf { get; set; }
+        public string? Email { get; set; }
+        public string? Senha { get; set; }
+        public string? Telefone { get; set; }
+        public string? Materia { get; set; }
+    }
+}
